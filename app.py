d7e262da4dc8aed1852ae92385b716f4a4a6c1d7
javeedashraf555-netizen <javeedashraf555@gmail.com>
2026-02-07 15:50:25 +0500

    app.py

--- app.py
+++ app.py
@@ -51,7 +51,8 @@ async def json_to_proto(json_data: str, proto_message: Message) -> bytes:
 
 def get_account_credentials(region: str) -> str:
     r = region.upper()
-    if r == "IND":
+    # Yahan PK ko IND ke saath add karein taaki Pakistan ki requests process ho sakein
+    if r in {"IND", "PK", "BD"}: 
         return "uid=3933356115&password=CA6DDAEE7F32A95D6BC17B15B8D5C59E091338B4609F25A1728720E8E4C107C4"
     elif r in {"BR", "US", "SAC", "NA"}:
         return "uid=4044223479&password=EB067625F1E2CB705C7561747A46D502480DC5D41497F4C90F3FDBC73B8082ED"
